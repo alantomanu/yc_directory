1e391026c4f102619b9205a62fb7e793613d5be9
Alanto Manu <alantomanu501@gmail.com>
2025-01-06 23:51:17 +0530

    Refactor navbar component: replace static header with dynamic navigation using Next.js Link and Image components. Remove unused SVG files to clean up the project.

--- yc_app/app/components/navbar.tsx
+++ yc_app/app/components/navbar.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
-
+import Link from 'next/link'
+import Image from 'next/image'
 const navbar = () => {
   return (
-    <div>
-        <h1>
-            Navbar
-        </h1>
+        <header className="py-5 px-3 bg-white shadow-sm font-work-sans">
+            <nav className="flex justify-between items-center">
+                <Link href="/"></Link>
+                <Image src="/logo.png" alt="logo" width={144} height={30}/>
+
+            </nav>
       
-    </div>
+    </header>
   )
 }
 
