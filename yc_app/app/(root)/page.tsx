8983430c4d6f87cb8d82db5df42d83ac38569810
Alanto Manu <alantomanu501@gmail.com>
2025-01-06 23:41:34 +0530

    Remove unused Home component from page.tsx  added navbar.tsx and add it to layout.tsx

---
+++ yc_app/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import Navbar from "../components/navbar";
+export default function Home() {
+  return (
+   <h1 className="text-2xl">
+    Home
+   </h1>
+  );
+}
---
+++ yc_app/app/components/navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+        <h1>
+            Navbar
+        </h1>
+      
+    </div>
+  )
+}
+
+export default navbar
