cca876709a6d877aab31ce1ce760d957b10d1c41
Alanto Manu <alantomanu501@gmail.com>
2025-01-07 14:57:14 +0530

    Enhance Navbar component with authentication: Implement dynamic user session handling, allowing users to log in via GitHub or sign out. Update layout to include user-specific links and improve overall navigation structure.

--- yc_app/app/components/navbar.tsx
+++ yc_app/app/components/navbar.tsx
@@ -1,17 +1,43 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-const navbar = () => {
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { auth } from '@/auth';
+
+const Navbar = async () => {
+  const session = await auth();
+
   return (
-        <header className="py-5 px-3 bg-white shadow-sm font-work-sans">
-            <nav className="flex justify-between items-center">
-                <Link href="/"></Link>
-                <Image src="/logo.png" alt="logo" width={144} height={30}/>
-
-            </nav>
-      
+    <header className="py-5 px-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>create</span>
+              </Link>
+              <form method="POST" action="/api/auth/signout">
+                <button type="submit">
+                  <span>sign out</span>
+                </button>
+              </form>
+              <Link href={`/users/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form method="POST" action="/api/auth/signin/github">
+              <button type="submit">
+                <span>Log in</span>
+              </button>
+            </form>
+          )}
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default navbar
+export default Navbar;
