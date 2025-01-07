b1a83bf6d45b36e93ca70a18ecb592c21dba17e0
Alanto Manu <alantomanu501@gmail.com>
2025-01-07 15:52:01 +0530

    Refactor Navbar component: Update authentication handling by integrating signIn and signOut functions. Improve user session management and enhance the layout for better navigation experience.

--- yc_app/app/components/navbar.tsx
+++ yc_app/app/components/navbar.tsx
@@ -1,43 +1,50 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import { auth } from '@/auth';
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import {auth, signIn, signOut as signOutAuth} from '@/auth'
 
-const Navbar = async () => {
-  const session = await auth();
-
+const navbar = async() => {
+  const session = await auth()
   return (
-    <header className="py-5 px-3 bg-white shadow-sm font-work-sans">
-      <nav className="flex justify-between items-center">
-        <Link href="/">
-          <Image src="/logo.png" alt="logo" width={144} height={30} />
-        </Link>
-        <div className="flex items-center gap-5 text-black">
-          {session && session?.user ? (
-            <>
-              <Link href="/startup/create">
-                <span>create</span>
-              </Link>
-              <form method="POST" action="/api/auth/signout">
-                <button type="submit">
-                  <span>sign out</span>
-                </button>
-              </form>
-              <Link href={`/users/${session?.id}`}>
-                <span>{session?.user?.name}</span>
-              </Link>
-            </>
-          ) : (
-            <form method="POST" action="/api/auth/signin/github">
-              <button type="submit">
-                <span>Log in</span>
-              </button>
-            </form>
-          )}
-        </div>
-      </nav>
-    </header>
-  );
-};
+        <header className="py-5 px-3 bg-white shadow-sm font-work-sans">
+            <nav className="flex justify-between items-center">
+                <Link href="/"> <Image src="/logo.png" alt="logo" width={144} height={30}/></Link>
+                <div className="flex items-center gap-5 text-black">
+                   {session && session?.user?(
+                    <>
+                    <Link href="/startup/create">
+                    <span>create</span>
+                    </Link>
+                    <form  action={async()=>{
+                      "use server";
+                      await signOutAuth()
+                      }}>
+                      <button type='submit'>
+                    <span>Log out</span>
+                    </button>
+                      </form>
+                    <Link href={'/users/${session?.id}'}>
+                    <span>{session?.user?.name}</span>
+                    </Link>
+                    </>
+                   ):(
+                   
+                   <form action={async()=>{
+                    "use server";
+                    await signIn()
+                   }}>
+                    <button type='submit'>
+                    <span>Log in</span>
+                    </button>
+                   </form>
+                   
+                   )}
+                </div>
 
-export default Navbar;
+            </nav>
+      
+    </header>
+  )
+}
+
+export default navbar;
