c6719df225b9fd5d9db8cce68b4ef7c87fbf0206
Alanto Manu <alantomanu501@gmail.com>
2025-01-12 22:21:37 +0530

    Refactor Home component and SearchForm: Updated Home to handle async search parameters and pass the query to SearchForm for improved search functionality. This change enhances the user experience by allowing dynamic query handling in the search form.

--- yc_app/app/(root)/page.tsx
+++ yc_app/app/(root)/page.tsx
@@ -1,5 +1,8 @@
 import SearchForm from "../components/SearchForm";
-export default function Home() {
+export default async function Home({searchParams}:{
+  searchParams:Promise<{query:string}>
+}) {
+  const query = (await searchParams).query;
   return (
     <>
     <section className="pink_container pattern">
@@ -10,7 +13,7 @@ export default function Home() {
 <p className="subheading !max-w-3xl text-white">
   Submit Ideas, Vote on Pitches,and Get Noticed in virtual competitions
 </p>
-<SearchForm />
+<SearchForm query={query} />
     </section>
     </>
    
--- yc_app/app/components/SearchForm.tsx
+++ yc_app/app/components/SearchForm.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import SearchFormReset from './searchFormReset';
 
-const SearchForm = () => {
-    const query = "test";
+const SearchForm = ({query}:{query:string}) => {
+  
    
   return (
     <div>
