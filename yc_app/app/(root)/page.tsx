3e34769f0f1d0eb52d36103e5acacba775c38fcf
Alanto Manu <alantomanu501@gmail.com>
2025-01-12 23:44:33 +0530

    Enhance Home component to display search results: Added StartupCard component for rendering individual startup posts and implemented conditional rendering for search results. Improved layout structure by including a new section for displaying results or a no-results message.

--- yc_app/app/(root)/page.tsx
+++ yc_app/app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -6,7 +7,7 @@ export default async function Home({
   searchParams: { query?: string };
 }) {
   const query = searchParams?.query || ""; // Safely access the query parameter
-
+  
   return (
     <>
       <section className="pink_container pattern">
@@ -23,6 +24,16 @@ export default async function Home({
         <p className="text-30-semibold">
           {query ? `Search results for "${query}"` : "All Startups"}
         </p>
+        <ul className="mt-7 card_grid">
+        {posts?.length > 0?(
+          posts.map((post:StartupCardType,index:number)=>(
+            <StartupCard key={post?._id} post={post} />
+        ))
+      ):(
+        <p className="no-results">No startups found</p>
+      )}
+        </ul>
+        
       </section>
     </>
   );
---
+++ yc_app/components/StartupCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const StartupCard = ({post}:{post:StartupTypeCard}) => {
+  return (
+    <div>
+      <li >
+
+      </li>
+    </div>
+  )
+}
+
+export default StartupCard
